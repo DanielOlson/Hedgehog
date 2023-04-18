14e3b7d16e02930a6f8304fddc9437e197d41c2c
Daniel Olson <daniel.olson@me.com>
2023-04-18 14:24:10 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+mod grid;
+mod cell;
+
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, VirtualKeyCode},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use winit_input_helper::WinitInputHelper;
+use crate::grid::Grid;
+
+
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 250;
+
+const CELL_SIZE: f64 = 2.0;
+
+fn main() -> Result<(), Error> {
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        let scaled_size = LogicalSize::new(WIDTH as f64 * CELL_SIZE, HEIGHT as f64 * CELL_SIZE);
+        WindowBuilder::new()
+            .with_title("Hedgehog")
+            .with_inner_size(scaled_size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut screen = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    };
+
+    let mut grid = Grid::new(WIDTH as usize, HEIGHT as usize);
+
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::RedrawRequested(_) = event {
+            grid.update_grid();
+            grid.draw_grid(screen.frame_mut());
+            if let Err(_err) = screen.render() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        if input.update(&event) {
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            if let Some(size) = input.window_resized() {
+                if let Err(err) = screen.resize_surface(size.width, size.height) {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+            }
+        }
+
+        window.request_redraw();
+    });
+
+}
